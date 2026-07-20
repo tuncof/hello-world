bfaf093337054b57816b64089c247c09673e441e
tuncof <75937212+tuncof@users.noreply.github.com>
2026-07-20 22:06:43 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/irtcpservice/MainActivity.java
+++ app/src/main/java/com/example/irtcpservice/MainActivity.java
@@ -1,1 +1,21 @@
 
+package com.example.irtcpservice;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.Gravity;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        TextView textView = new TextView(this);
+        textView.setText("Hello World! GitHub Actions Çalışıyor.");
+        textView.setTextSize(24);
+        textView.setGravity(Gravity.CENTER);
+        
+        setContentView(textView);
+    }
+}
